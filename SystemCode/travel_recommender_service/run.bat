f6d07c616e2b2a9feac89470afee46e7a67ceae8
akimoto-cris <akimotohisashibi@outlook.com>
2019-09-22 09:43:10 +0800

    add lauch script for frontend and backend

--- SystemCode/travel_recommender_service/run.bat
+++ SystemCode/travel_recommender_service/run.bat
@@ -1,1 +1,40 @@
-mvn clean spring-boot:run
+@echo off
+
+set modeldir=travel_recommender_model
+set kjardir=travel-recommender
+set servicedir=travel_recommender_service
+set frontenddir=travel_recommender_frontend
+
+cd ..
+echo ==============================
+echo Entering %frontenddir%, compiling
+echo ==============================
+cd %frontenddir%
+call npm -v
+call ng -v
+call npm i -s
+start /b ng serve
+
+
+cd ..
+echo ==============================
+echo Entering %modeldir%, compiling
+echo ==============================
+cd %modeldir%
+call mvn clean install
+
+cd ..
+echo ==============================
+echo Entering %kjardir%, compiling
+echo ==============================
+cd %kjardir% 
+call mvn clean install
+
+cd ..
+echo ==============================
+echo Entering %servicedir%, running service
+echo ==============================
+cd %servicedir% 
+call mvn spring-boot:run
+
+cd ..
