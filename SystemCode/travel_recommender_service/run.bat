4955c0cfae14c14ae0993c59ce744ca1c0027c6c
akimoto-cris <akimotohisashibi@outlook.com>
2019-09-22 17:14:59 +0800

    add rules to make more genuine rostering

--- SystemCode/travel_recommender_service/run.bat
+++ SystemCode/travel_recommender_service/run.bat
@@ -1,39 +1,40 @@
 @echo off
 
+set bar=======================================================
 set modeldir=travel_recommender_model
 set kjardir=travel-recommender
 set servicedir=travel_recommender_service
 set frontenddir=travel_recommender_frontend
 
 cd ..
-echo ==============================
-echo Entering %frontenddir%, compiling
-echo ==============================
+echo %bar%
+echo Entering %frontenddir%, compiling Angular JS
+echo %bar%
 cd %frontenddir%
 call npm -v
 call npm i -s
-
+call npm install --save-dev @angular-devkit/build-angular
 call ng version
-start /b ng serve
+start CMD /C call ng serve
 
 cd ..
-echo ==============================
+echo %bar%
 echo Entering %modeldir%, compiling
-echo ==============================
+echo %bar%
 cd %modeldir%
 call mvn clean install
 
 cd ..
-echo ==============================
+echo %bar%
 echo Entering %kjardir%, compiling
-echo ==============================
+echo %bar%
 cd %kjardir% 
 call mvn clean install
 
 cd ..
-echo ==============================
+echo %bar%
 echo Entering %servicedir%, running service
-echo ==============================
+echo %bar%
 cd %servicedir% 
 call mvn spring-boot:run
 
